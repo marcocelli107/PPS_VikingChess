559b127a5ce58db638e7a8d38e2f8c6c059ac947
lucanannini <luca.nannini2@studio.unibo.it>
2020-06-19 09:37:10 +0200

    Added Board toString

--- src/main/scala/utils/BoardGame.scala
+++ src/main/scala/utils/BoardGame.scala
@@ -1,74 +1,77 @@
 package utils
 
+import model.Piece
 import model.Piece.PieceType
+import utils.BoardGame.{Board, BoardCell}
+
+import scala.collection.mutable.ListBuffer
 
 case class Coordinate(x: Int, y: Int)
 
 object BoardGame {
 
   trait BoardCell {
-    /**
-      * Coordinate of the cell
-      */
-    //def coordinates: Coordinate
 
     /**
-      * Piece in the cell
-      */
-    //def piece: PieceType
-
-    /**
-      * Gets piece in the cell
-      */
+     * Gets piece in the cell
+     */
     def getPiece: PieceType
 
     /**
-      * Gets coordinate of the cell
-      */
+     * Gets coordinate of the cell
+     */
     def getCoordinate: Coordinate
+
+    def toString: String
   }
 
   object BoardCell {
 
     def apply(coordinateCell: Coordinate, piece: PieceType): BoardCell = BoardCellImpl(coordinateCell, piece)
 
-    case class BoardCellImpl(coordinateCell: Coordinate, pieceCell: PieceType) extends BoardCell {
-
-      //override def coordinates: Coordinate = coordinateCell
-
-      //override def piece: PieceType = pieceCell
-      //private val coordinate: Coordinate = coordinateCell
-
-      //private val piece: PieceType = pieceCell
+    case class BoardCellImpl(private val coordinateCell: Coordinate, private val pieceCell: PieceType) extends BoardCell {
 
       override def getPiece: PieceType = pieceCell
 
       override def getCoordinate: Coordinate = coordinateCell
+
+      override def toString: String = {
+        var p: String = ""
+        /* TODO USARE ENUM */
+        if (pieceCell.equals(Piece.Void)) p = "e"
+        if (pieceCell.equals(Piece.WhitePawn)) p = "wp"
+        if (pieceCell.equals(Piece.BlackPawn)) p = "bp"
+        if (pieceCell.equals(Piece.WhiteKing)) p = "wk"
+        "cell(coord(" + coordinateCell.x + "," + coordinateCell.y + ")," + p + ")"
+      }
     }
+
   }
 
   trait Board {
     /**
-      * Defines board's cells list.
-      */
+     * Defines board's cells list.
+     */
     def cells: Seq[BoardCell]
 
     /**
-      * Defines size of board's side.
-      */
+     * Defines size of board's side.
+     */
     def size: Int
 
     /**
-      * Gets a cell in the board from a coordinate.
-      */
+     * Gets a cell in the board from a coordinate.
+     */
     def getCell(coordinate: Coordinate): BoardCell
+
+    def toString: String
   }
 
   object Board {
 
     def apply(cells: Seq[BoardCell]): Board = BoardImpl(cells)
 
-    case class BoardImpl(allCells: Seq[BoardCell]) extends Board {
+    case class BoardImpl(private val allCells: Seq[BoardCell]) extends Board {
 
       override def cells: Seq[BoardCell] = allCells
 
@@ -77,6 +80,21 @@ object BoardGame {
       override def getCell(coordinate: Coordinate): BoardCell = allCells.filter(_.getCoordinate == coordinate).head
 
       override def equals(obj: Any): Boolean = this.cells.equals(obj.asInstanceOf[Board].cells)
+
+      override def toString: String = cells.map(_.toString).grouped(size).toList.map(_.mkString("[", ",", "]")).mkString("[", ",", "]")
+    }
+
+  }
+
+}
+
+object prova extends App {
+  var s: ListBuffer[BoardCell] = ListBuffer.empty
+  for (x <- 1 to 11) {
+    for (y <- 1 to 11) {
+      s += BoardCell(Coordinate(x, y), Piece.Void)
     }
   }
+  var b: Board = Board(s)
+  println(b.toString)
 }
